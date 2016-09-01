849469011722732b0cd730025427172af3b47e41
csrster <csr@statsbiblioteket.dk>
2016-09-01 16:49:58 +0200

    Now generates ifiles.

--- scripts/precompress.sh
+++ scripts/precompress.sh
@@ -38,17 +38,10 @@ for i in $(seq 1 ${#JOBNR})
 do
     OUTPUT_DIR=$OUTPUT_DIR/${JOBNR:i-1:1}
 done
-$DEBUB && echo Output dir is $OUTPUT_DIR
+$DEBUG && echo Output dir is $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
 
-# Create a cdx file for the input file
-OCDX=$OUTPUT_DIR/$(basename "$1").cdx
-$DEBUG && echo Creating cdx $OCDX
-$JWAT_DIR/jwattools.sh cdx -o $OCDX $INPUT_FILE
-if [ $? -ne 0 ]; then
-    func_exit "Error creating cdx file $OCDX from $INPUT_FILE." 3
-fi
 
 # Calculate the sha1 for the input file
 OSHA1=$(sha1sum $1 |cut -d ' ' -f 1)
@@ -66,14 +59,37 @@ if [ $? -eq 0 ];  then
 else
      func_exit "Could not create compressed file $OUTPUT_FILE from $INPUT_FILE." 4
 fi
+
+# Check that the file can be reinflated
+TEMP_FILE=$(mktemp)
+gunzip $OUTPUT_FILE --to-stdout > $TEMP_FILE
+NSHA1=$(sha1sum $TEMP_FILE |cut -d ' ' -f 1)
+if [ "$NSHA1" != "$OSHA1" ]; then
+   func_exit "SHA1 mismatch on decompression between $INPUT_FILE and $TEMP_FILE."
+else
+   $DEBUG && echo "Checksum match confirmed for $INPUT_FILE."
+   rm $TEMP_FILE
+fi
+
 # Calculate the md5 for the output file and store
-echo $(basename $OUTPUT_FILE)"##"$(openssl dgst -md5 $OUTPUT_FILE | cut -d'=' -f 2|cut -d' ' -f 2)   >>$CHECKSUM_FILE
+echo $(basename $OUTPUT_FILE)"##"$(md5sum $OUTPUT_FILE | cut -d' ' -f 1)   >>$CHECKSUM_FILE
+
+# Create a cdx file for the input file
+OCDX=$OUTPUT_DIR/$(basename "$1").cdx
+$DEBUG && echo Creating cdx $OCDX
+$JWAT_DIR/jwattools.sh cdx -o $OCDX $INPUT_FILE
+if [ $? -ne 0 ]; then
+    func_exit "Error creating cdx file $OCDX from $INPUT_FILE." 3
+fi
 
 # Create a cdx file for the output file
 NCDX=${OUTPUT_FILE}.cdx
 $JWAT_DIR/jwattools.sh cdx -o $NCDX $OUTPUT_FILE
 if [ $? -ne 0 ]; then
   func_exit "Error creating cdx file $NCDX from $OUTPUT_FILE." 5
+else
+  $DEBUG && echo "Deleting output file $OUTPUT_FILE."
+  rm $OUTPUT_FILE
 fi
 
 # Compare the line count of the two cdx files
@@ -83,11 +99,20 @@ if [ $LOCDX -ne $LNCDX ]; then
     func_exit "Output and input files have different numbers of records ($LNCDX vs. $LOCDX)." 6
 fi
 
-# Create a temporary decompressed file
-
-# Compare the decompressed file with the original
-
-# Delete the original
+# Generate the i-file name
+IFILE=$OUTPUT_DIR/$(basename "$INPUT_FILE").ifile.cdx
+$DEBUG && echo "Creating ifile $IFILE."
+rm -f $IFILE
 
 # Loop over the two cdx files to create the lookup cdx
-
+while read ocdx_line <&3 && read ncdx_line <&4; do
+if [[ ! "$ocdx_line" =~ CDX ]]; then
+     echo $(echo $ocdx_line| rev |cut -d ' ' -f 2|rev )" "$(echo $ncdx_line|rev|cut -d ' ' -f 2|rev )" "$(echo $ncdx_line|cut -d ' ' -f 2 ) >>$IFILE
+fi
+done 3<$OCDX 4<$NCDX
+if [ $? -ne 0 ]; then
+   func_exit "Error creating  ifile $IFILE." 8
+else
+   $DEBUG && echo "Cleaning up."
+   rm $NCDX $OCDX
+fi
