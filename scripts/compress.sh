b31d7655f326c6114a3f11a04e01a9a7fce55c14
Netarkiv <netarkiv@iapetus.statsbiblioteket.dk>
2016-11-21 14:16:04 +0100

    Functioning compression script.

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -51,7 +51,39 @@ if ! [ -f $INPUT_FILE ]; then
      func_exit "No such file $INPUT_FILE" 2
 fi
 
+if ! [ -f $CHECKSUM_FILE ]; then
+     func_exit "No such file $CHECKSUM_FILE" 2
+fi
+
+BASEFILE=$(basename $INPUT_FILE)
+## How badly does the next line scale?
+MD5_EXPECTED=$(grep $BASEFILE $CHECKSUM_FILE |cut -d'#' -f 3)
+MD5_EXPECTED=$(trim $MD5_EXPECTED)
+if [ -z "$MD5_EXPECTED" ]; then
+     func_exit "No target checksum for $BASEFILE in $CHECKSUM_FILE" 3
+fi
 
 
 
 
+$JWAT_DIR/jwattools.sh --verify compress $JWAT_COMPRESSION $INPUT_FILE
+OUTPUT_FILE="$INPUT_FILE".gz
+
+if ! [ -f $OUTPUT_FILE ]; then
+     func_exit "Output file $OUTPUT_FILE not generated." 3
+fi
+
+MD5_ACTUAL=$( md5sum $OUTPUT_FILE |cut -d' ' -f 1)
+
+if [ "$MD5_ACTUAL" == "$MD5_EXPECTED" ]; then
+(
+  flock -x -w 10 202 || exit 1
+  ## rm $INPUT_FILE
+  echo "Created $OUTPUT_FILE . Deleted $INPUT_FILE ." >>$LOG
+) 202>$LOCK
+else
+(
+  flock -x -w 10 202 || exit 1
+  echo "Created $OUTPUT_FILE . Did not delete $INPUT_FILE because of md5sum mismatch." >>$LOG
+) 202>$LOCK
+fi
