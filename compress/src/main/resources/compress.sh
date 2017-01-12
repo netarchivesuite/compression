cb8225c66e5334f17e7640bcdf0962056a90c7fc
csrster <csr@statsbiblioteket.dk>
2017-01-12 10:09:39 +0100

    Scripts for compression.

---
+++ compress/src/main/resources/compress.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PACKAGE=dk.nationalbiblioteket.netarkivet.compression.compression
+CLASS=Compressor
+java -cp "$DIR/../lib/"'*' -Dconfig=$DIR/../config/compress.conf $PACKAGE.$CLASS $1
