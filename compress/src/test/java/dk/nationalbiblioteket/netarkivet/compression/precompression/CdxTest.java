280620d829196ff329fb09335cce08844d989337
Colin Rosenthal <csr@statsbiblioteket.dk>
2017-06-08 09:09:55 +0200

    Testdata cleanup

--- compress/src/test/java/dk/nationalbiblioteket/netarkivet/compression/precompression/CdxTest.java
+++ compress/src/test/java/dk/nationalbiblioteket/netarkivet/compression/precompression/CdxTest.java
@@ -1,6 +1,8 @@
 package dk.nationalbiblioteket.netarkivet.compression.precompression;
 
+import dk.nationalbiblioteket.netarkivet.compression.metadata.MetadatafileGeneratorRunnableTest;
 import org.jwat.tools.tasks.cdx.CDXFile;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
 
@@ -11,9 +13,14 @@ import static org.testng.Assert.assertTrue;
  */
 public class CdxTest extends CDXFile {
 
+    @BeforeMethod
+    public void setup() throws Exception {
+        MetadatafileGeneratorRunnableTest.setup();
+    }
+
     @org.testng.annotations.Test
     public void testCdxGen() {
-        File input = new File("src/test/data/1185-77-20110304134905-00003-kb-test-har-002.kb.dk.arc");
+        File input = new File("src/test/data/WORKING/1185-77-20110304134905-00003-kb-test-har-002.kb.dk.arc");
         CDXFile cdxFile = new CDXFile();
         cdxFile.processFile(input);
 
