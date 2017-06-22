a599b84bb0577fbad6d8ccad11ab66f98836a2de
Colin Rosenthal <csr@statsbiblioteket.dk>
2017-06-22 12:04:12 +0200

    Sorting script.

---
+++ src/main/resources/cdx_sort.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+print_usage()
+{
+    echo "Usage: $(basename $0) -d [-t <tmpdir>] -o <outputfile> dir"
+    echo
+    echo "Recursively merges/sorts files under dir"
+    echo
+    echo "-d delete source files"
+    echo "-t tempdir for sorting"
+    echo "-o output file"
+}
+
+delete=0
+tempdir=$(dirname $(mktemp -u))
+while  getopts ":t:o:d" opt; do
+    case $opt in
+       d)
+          delete=1
+          ;;
+       t)
+          tempdir=$OPTARG
+          ;;
+       o)
+          output=$OPTARG
+          ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+dir=$1
+
+[ -z "$dir" ] && print_usage && exit 1
+[ -z "$output" ] && print_usage && exit 1
+
+tempfile=$(mktemp)
+echo "Sorting files in $dir to $output, using temporary file $tempfile."
+
+export LC_ALL=C;
+find $dir -type f -exec bash -c "cat '{}' >>$tempfile; if [ $delete -eq 1 ]; then rm '{}'; fi" \;
+sort -T $tempdir $tempfile > $output
+rm $tempfile
+
