a29315b4f5d19486fc932e658c8ada045e62e047
Colin Rosenthal <csr@statsbiblioteket.dk>
2017-06-29 15:27:38 +0200

    More flexible script with regex restriction and multiple directories.

--- src/main/resources/cdx_sort.sh
+++ src/main/resources/cdx_sort.sh
@@ -2,18 +2,22 @@
 
 print_usage()
 {
-    echo "Usage: $(basename $0) -d [-t <tmpdir>] -o <outputfile> dir"
+    echo "Usage: $(basename $0) -d [-t <tmpdir>] [-e <regex>] -o <outputfile> dir1 dir2 ..."
     echo
     echo "Recursively merges/sorts files under dir"
     echo
     echo "-d delete source files"
     echo "-t tempdir for sorting"
+    echo "-e regex matching filenames to be included, e.g. -e '1[0-4]*' (default value '*')"
     echo "-o output file"
+    echo
+    echo "Example: ./cdx_sort.sh -t /tmp -e '*.txt' -o sorted.txt mydir yourdir"
 }
 
 delete=0
+regex='*'
 tempdir=$(dirname $(mktemp -u))
-while  getopts ":t:o:d" opt; do
+while  getopts ":t:o:e:d" opt; do
     case "$opt" in
        d)
           delete=1
@@ -24,6 +28,9 @@ while  getopts ":t:o:d" opt; do
        o)
           output="$OPTARG"
           ;;
+       e)
+          regex="$OPTARG"
+          ;;
        ?)
           print_usage
           exit 1
@@ -32,15 +39,15 @@ while  getopts ":t:o:d" opt; do
 done
 
 shift $(($OPTIND - 1))
-dir=$1
+dirs=$@
 
-[ -z "$dir" ] && print_usage && exit 1
+[ -z "$dirs" ] && print_usage && exit 1
 [ -z "$output" ] && print_usage && exit 1
 
 tempfile=$(mktemp)
-echo "Sorting files in $dir to $output, using temporary file $tempfile."
+echo "Sorting files in $dirs matching $regex to $output, using temporary file $tempfile."
 
-find $dir -type f -exec bash -c "cat '{}' >>$tempfile; if [ $delete -eq 1 ]; then rm '{}'; fi" \;
+find $dirs -type f -name "$regex" -exec bash -c "cat '{}' >>$tempfile; if [ $delete -eq 1 ]; then rm '{}'; fi" \;
 LC_ALL=C sort -T $tempdir $tempfile > $output
 rm $tempfile
 
