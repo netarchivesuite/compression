26c3c00d947aed453792e9db3fc4d2447b483ac6
Toke Eskildsen <toes@kb.dk>
2017-10-10 15:01:08 +0200

    Implemented parallel loads of offsetMaps

--- src/main/java/dk/nationalbiblioteket/netarkivet/compression/metadata/ifilecache/objectbased/IFileCacheImpl.java
+++ src/main/java/dk/nationalbiblioteket/netarkivet/compression/metadata/ifilecache/objectbased/IFileCacheImpl.java
@@ -6,9 +6,7 @@ import dk.nationalbiblioteket.netarkivet.compression.metadata.ifilecache.IFileCa
 import java.io.FileNotFoundException;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.*;
 
 /**
  *
@@ -16,19 +14,23 @@ import java.util.concurrent.ConcurrentSkipListMap;
 public class IFileCacheImpl implements IFileCache {
 
     private static IFileCacheImpl instance;
-
     private IFileLoader iFileLoader;
-
-    private ArrayBlockingQueue<String> elementQueue;
-
-
-    private ConcurrentHashMap<String, ConcurrentSkipListMap<Long, IFileEntry>> cache =
-            new ConcurrentHashMap<String, ConcurrentSkipListMap<Long, IFileEntry>>();
-
+    private final ArrayBlockingQueue<String> elementQueue;
+    private ConcurrentHashMap<String, Future<ConcurrentSkipListMap<Long, IFileEntry>>> cache = new ConcurrentHashMap<>();
+    // TODO: 40 threads are pretty arbitrary - maybe make this a property?
+    private final ExecutorService executor = Executors.newFixedThreadPool(40, new ThreadFactory() {
+        private int loaderID = 0;
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r, "iFileLoader_" + loaderID++);
+            t.setDaemon(true);
+            return t;
+        }
+    });
 
     private IFileCacheImpl(IFileLoader iFileLoader) {
         int cacheSize = Integer.parseInt(Util.getProperties().getProperty(Util.CACHE_SIZE));
-        elementQueue = new ArrayBlockingQueue<String>(cacheSize);
+        elementQueue = new ArrayBlockingQueue<>(cacheSize);
         this.iFileLoader = iFileLoader;
     }
 
@@ -39,47 +41,69 @@ public class IFileCacheImpl implements IFileCache {
         return instance;
     }
 
-
-    private Map<Long,IFileEntry> loadFile(String filename) throws FileNotFoundException {
-        synchronized (elementQueue) {
-            ConcurrentSkipListMap<Long, IFileEntry> newMap = iFileLoader.getIFileEntryMap(filename);
-            if (elementQueue.remainingCapacity() == 0) {
-                String evictedFilename = elementQueue.poll();
-                cache.remove(evictedFilename);
-            }
-            elementQueue.add(filename);
-            cache.put(filename, newMap);
-            return newMap;
-        }
-    }
-
-
     @Override
     public IFileEntry getIFileEntry(String oldFilename, Long oldOffset) throws FileNotFoundException {
-        synchronized (elementQueue) {
-            final ConcurrentSkipListMap<Long, IFileEntry> offsetMap = cache.get(oldFilename);
-            if (offsetMap == null) {
-                return loadFile(oldFilename).get(oldOffset);
-            } else {
-                return offsetMap.get(oldOffset);
-            }
-        }
+        return getOffsetMap(oldFilename).get(oldOffset);
     }
 
     @Override
     public Iterator<Map.Entry<Long, IFileEntry>> getOrderedListing(String oldFilename) throws FileNotFoundException {
-        synchronized (elementQueue) {
-            final ConcurrentSkipListMap<Long, IFileEntry> offsetMap = cache.get(oldFilename);
-            if (offsetMap == null) {
-                return loadFile(oldFilename).entrySet().iterator();
-            } else {
-                return offsetMap.entrySet().iterator();
-            }
-        }
+        return getOffsetMap(oldFilename).entrySet().iterator();
     }
 
     @Override
     public int getCurrentCachesize() {
         return cache.size();
     }
+
+    private Map<Long, IFileEntry> getOffsetMap(String oldFilename) throws FileNotFoundException {
+        Future<ConcurrentSkipListMap<Long, IFileEntry>> offsetMap;
+
+        synchronized (elementQueue) {
+            offsetMap = cache.get(oldFilename);
+
+            if (offsetMap == null) {
+                // We need to load a new entry. Make sure that there is enough room in the cache first
+                if (elementQueue.remainingCapacity() == 0) {
+                    String evictedFilename = elementQueue.poll();
+                    cache.remove(evictedFilename);
+                }
+                elementQueue.add(oldFilename);
+
+                // Create & activate a future and return the result from outside of the synchronization block
+                offsetMap = executor.submit(new CallableLoader(oldFilename));
+                cache.put(oldFilename, offsetMap);
+            }
+        }
+
+        // We have a Future which either has an offsetMap or will at some point have one (or fail)
+        // get blocks, so we simply return the result from that
+        try {
+            return offsetMap.get();
+        } catch (InterruptedException e) {
+            FileNotFoundException notFound = new FileNotFoundException(
+                    "Interrupted while waiting for loading of entry for '" + oldFilename + "'");
+            notFound.initCause(e);
+            throw notFound;
+        } catch (ExecutionException e) {
+            FileNotFoundException notFound = new FileNotFoundException(
+                    "Internal error: ExecutionException while loading entry '" + oldFilename + "'");
+            notFound.initCause(e);
+            throw notFound;
+        }
+    }
+
+    private class CallableLoader implements Callable<ConcurrentSkipListMap<Long, IFileEntry>> {
+        private final String oldFileName;
+
+        public CallableLoader(String oldFileName) {
+            this.oldFileName = oldFileName;
+        }
+
+        @Override
+        public ConcurrentSkipListMap<Long, IFileEntry> call() throws Exception {
+            return iFileLoader.getIFileEntryMap(oldFileName);
+        }
+    }
+
 }
