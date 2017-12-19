24854f91674ce431cfe951cad3e9169e5f80489f
Nicholas Clarke <nicl@kb.dk>
2017-12-19 16:16:05 +0100

    Reduce CDX verify memory usage.

--- src/main/resources/precompress.sh
+++ src/main/resources/precompress.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKAGE=dk.nationalbiblioteket.netarkivet.compression.precompression
 CLASS=PreCompressor
-java -cp "$DIR/../lib/"'*' -Dconfig=$DIR/../config/precompress.conf $PACKAGE.$CLASS $1 $2
+java -cp "$DIR/../lib/"'*' -Dlogback.configurationFile=$DIR/../config/precompress.logback.xml -Dconfig=$DIR/../config/precompress.conf $PACKAGE.$CLASS $1 $2
