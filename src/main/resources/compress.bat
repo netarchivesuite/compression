bf076d402ae332fd8c454638d09bcc1f1e8bf131
csrster <csr@statsbiblioteket.dk>
2018-03-22 15:14:31 +0100

    New compress script with splitting.

--- src/main/resources/compress.bat
+++ src/main/resources/compress.bat
@@ -1,10 +1,24 @@
+IF [%2]==[] GOTO NO_ARGUMENT
+
+cscript %~dp0\split.vbs %1 %2
+
 Set PACKAGE=dk.nationalbiblioteket.netarkivet.compression.compression
 Set CLASS=Compressor
-
 Set JARDIR=%~dp0\..\lib
 Set CONFDIR=%~dp0\..\config
 
-java -classpath "%JARDIR%/*" -Dconfig="%CONFDIR%/compress.conf" %PACKAGE%.%CLASS% %1 %2
+for %%f in (split_output_*)  do (
+   java -classpath "%JARDIR%/*" -Dconfig="%CONFDIR%/compress.conf" %PACKAGE%.%CLASS% %%f %3
+   del %%f
+   ECHO "Finished processing corpus file %%f"
+)
 
-echo Finished compression
+ECHO "Finished all processing"
 
+GOTO DONE
+
+:NO_ARGUMENT
+ECHO "Usage: compress.bat <corpus_file> <chunksize> <blacklistfile>"
+exit /B 1
+
+:DONE
